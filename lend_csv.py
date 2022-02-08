c832c874c15e6563535aadd1f748fd6c13f1a21b
Rafael-Prochnow <rafaprochnow31@gmail.com>
2022-02-08 14:27:21 -0300

    CSV

---
+++ lend_csv.py
@@ -0,0 +1,10 @@
+"""
+Lendo arquivos csv
+CSV - Comma Separeted Values - Valores Separados por Virgula
+
+with open('lutadores.csv', encoding="utf8") as arquivo:
+    dados = arquivo.read()
+    print(type(dados))
+    print(dados)
+"""
+
